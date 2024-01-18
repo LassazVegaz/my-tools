a2c724c23f735af5ca5eb09db0542f1a94ba25fc
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-18 23:09:26 +0530

    some ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
+import { Box, Button, Container, TextField } from "@mui/material";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <Box height="100vh" display="grid" gridTemplateRows="1fr auto">
+      <Box></Box>
+
+      <Box
+        bgcolor="#EAECFF"
+        boxShadow="0px 0px 9.9px 0px #6472FF"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        height="20vh"
+        gap={2}
+      >
+        <TextField size="small" type="date" sx={{ width: 200 }} />
+        <TextField
+          size="small"
+          sx={{
+            width: 130,
+            "& input": {
+              textAlign: "center",
+            },
+          }}
+        />
+        <Button variant="outlined">Add</Button>
+      </Box>
+    </Box>
+  );
 }
