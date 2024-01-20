9a8f2353b594d778334ecfb69e6f0a62e1f28fb4
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-20 19:48:34 +0530

    messages

--- src/app/components/AddForm.hook.ts
+++ src/app/components/AddForm.hook.ts
@@ -1,6 +1,7 @@
 import { useFormik } from "formik";
 import { initialValues, validationSchema } from "./AddForm.helper";
 import { submitForm } from "./AddForm.actions";
+import { toast } from "react-toastify";
 
 const useAddFormUtils = () => {
   const form = useFormik({
@@ -10,7 +11,9 @@ const useAddFormUtils = () => {
       try {
         await submitForm(form.values);
         form.resetForm();
+        toast.success("Worked hours added successfully.");
       } catch (error) {
+        toast.error("Adding worked hours failed. Please try again.");
         console.error(error);
       }
     },
