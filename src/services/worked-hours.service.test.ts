c04d464ea233a042082135957364cca0d97d8002
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-20 11:38:48 +0530

    some stupid tests

--- src/services/worked-hours.service.test.ts
+++ src/services/worked-hours.service.test.ts
@@ -1,9 +1,21 @@
-import { expect, test } from "@jest/globals";
+import { describe, expect, test } from "@jest/globals";
 import { WorkedHoursService } from "./worked-hours.service";
 import prismaMock from "@/mocks/prisma.mock";
 
-test("worked hours service creation", () => {
-  let service: WorkedHoursService | undefined;
-  expect(() => (service = new WorkedHoursService(prismaMock))).not.toThrow();
-  expect(service).toBeDefined();
+describe("worked hours service", () => {
+  test("worked hours service creation", () => {
+    let service: WorkedHoursService | undefined;
+    expect(() => (service = new WorkedHoursService(prismaMock))).not.toThrow();
+    expect(service).toBeDefined();
+  });
+
+  test("add worked hours", async () => {
+    const workedHours = 8;
+    const date = new Date();
+    const service = new WorkedHoursService(prismaMock);
+
+    expect(
+      async () => await service.addWorkedHours(workedHours, date)
+    ).not.toThrow();
+  });
 });
