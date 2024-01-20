cea4d8fdf62205b69814112e59f1e0be8f9a73e4
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-20 10:42:50 +0530

    jest configs to resolve paths

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,8 @@
  */
 
 import type { Config } from "jest";
+import { pathsToModuleNameMapper } from "ts-jest";
+import tsconfig from "./tsconfig.json";
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -68,9 +70,6 @@ const config: Config = {
   //   "json",
   //   "node"
   // ],
-
-  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -196,6 +195,11 @@ const config: Config = {
 
   // A preset that is used as a base for Jest's configuration
   preset: "ts-jest",
+
+  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
 };
 
 export default config;
