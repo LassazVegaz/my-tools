7d4e14b3c425053b74fe13276f04ff45e0104a71
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-21 17:38:17 +0530

    disable PWA in dev

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import withPWAInit from "@ducanh2912/next-pwa";
 
-const withPWA = withPWAInit();
+const withPWA = withPWAInit({
+  disable: process.env.NODE_ENV === "development",
+});
 
 export default withPWA();
