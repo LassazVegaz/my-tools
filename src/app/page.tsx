cdd9dd13fa63e5bcd75885fcbf860aa1225acb1c
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-21 14:02:17 +0530

    async removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import { ClientComponent } from "./components";
 
-export default async function Home() {
+export default function Home() {
   return (
     <Box height="100vh" display="grid" gridTemplateRows="1fr auto">
       <ClientComponent />
