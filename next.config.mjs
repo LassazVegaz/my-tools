e01f0245a6afd14d6a16540a4be83c318e89576e
LassazVegaz <lasindunuwanga@gmail.com>
2024-04-28 21:09:20 +0530

    most stuff about local deploy removed

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,4 @@ const withPWA = withPWAInit({
   disable: process.env.NODE_ENV === "development",
 });
 
-export default withPWA({
-  distDir: process.env.DEPLOY === "1" ? "build" : undefined,
-});
+export default withPWA();
