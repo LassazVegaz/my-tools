2c4b45f7b665382167e9fdf974d6533021d77eae
LassazVegaz <lasindunuwanga@gmail.com>
2024-05-07 10:27:03 +0530

    use of numbers instead of dates chart

--- src/app/components/Chart.tsx
+++ src/app/components/Chart.tsx
@@ -9,10 +9,15 @@ type ChartProps = {
 };
 
 const formatWorkedHours = (workedHours: WorkedHours[]) =>
-  workedHours.map((workedHour) => ({
-    hours: workedHour.hours,
-    dateLabel: `${workedHour.date.getDate()}/${workedHour.date.getMonth() + 1}`,
-  }));
+  workedHours.map((workedHour) => {
+    const dateStr = workedHour.date.toString(); // YYYYMMDD
+    const day = parseInt(dateStr.slice(6, 8));
+    const month = parseInt(dateStr.slice(4, 6));
+    return {
+      hours: workedHour.hours,
+      dateLabel: `${day}/${month}`,
+    };
+  });
 
 const Chart = (props: ChartProps) => {
   const theme = useTheme();
